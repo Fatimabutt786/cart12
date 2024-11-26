b087e13ff9afe7f4f339085c2bca15ffb72288a4
Fatima Butt <168910378+Fatimabutt786@users.noreply.github.com>
2024-11-26 22:33:46 +0500

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,74 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "AJMS-13 EMBROIDERED KOREAN RAW SILK 3 PCS",
+      price: 30990.00,
+      img: "shop1.webp",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "AJMBP-07 - 2PCS",
+      price: 4490.00,
+      img: "shop2.webp",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "AJMBP-02 - 2PCS",
+      price: 3592,
+      
+      img: "shop3.webp",
+    },
+    {
+      id: "thyfhcbcvw",
+      name: "PRINTED - 2 PC LAWN SUIT",
+      price: 2400.00,
+     
+      img: "shop9.webp",
+    },
+    {
+        id: "thyfhcbcvc",
+        name: "AJMS-14 EMBROIDERED INDIAN RAW SILK 3 PCS",
+        price: 34990,
+        
+        img: "shop4.webp",
+      },{
+        id: "thyfhcbcvo",
+        name: "Men Unstitched Wash & Wear Suit - Summer Superior",
+        price: 4499,
+        img: "shop5.webp",
+      },{
+        id: "thyfhcbcva",
+        name: "3PC Unstitched Khaddar Suit KKH-2251",
+        price: 4699,
+        img: "shop6.webp",
+      },{
+        id: "thyfhcbcvb",
+        name: "3PC Unstitched Khaddar Suit KKH-2252",
+        price: 4699,
+        img: "shop7.webp",
+      },{
+        id: "thyfhcbcvh",
+        name: "PRINTED - 2 PC LAWN SUIT",
+        price: 2500,
+        img: "shop8.webp",
+      },{
+        id: "thyfhcbcvd",
+        name: "2 Piece Printed Cambric Suit with Lawn Dupatta",
+        price: 500,
+        
+        img: "shop10.webp",
+      },{
+        id: "thyfhcbcvjg",
+        name: "PRINTED - 2 PC LAWN SUIT",
+        price: 2125,
+        
+        img: "shop11.webp",
+      },
+      {
+        id: "thyfhcbcvjf",
+        name: "PRINTED - 2 PC LAWN SUIT",
+        price: 2275,
+        
+        img: "shop12.webp",
+      },
+  ];
